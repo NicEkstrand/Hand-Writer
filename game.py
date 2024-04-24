39c3f5416ab55d59ba4a2dde7f456204483312eb
nicolasekstrand <nicolas.ekstrand@menloschool.org>
2024-04-24 11:52:54 -0700

    Finished Camera Setup

--- game.py
+++ game.py
@@ -7,8 +7,10 @@ video = cv2.VideoCapture(0)
 # Loop until the end of the video
 while(video.isOpened()):
 
+    frame = video.read()[1]
+
     # Display the resulting frame
-    cv2.imshow("Smiles")
+    cv2.imshow("Smiles", frame)
 
     # Define q as the exit button
     if cv2.waitKey(50) & 0xFF == ord("q"):
