8ab366868aa54a71487a779dc9372a185c929601
nicolasekstrand <nicolas.ekstrand@menloschool.org>
2024-05-01 13:27:12 -0700

    Added beginning color/thickness customization

--- game.py
+++ game.py
@@ -14,65 +14,6 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 DrawingUtil = mp.solutions.drawing_utils
 
-
-"""
-Letter Detection Function written by Geeks for Geeks
-Link: https://www.geeksforgeeks.org/text-detection-and-extraction-using-opencv-and-ocr/#
-Edited by Nic Ekstrand
-
-"""
-
-"""def letter_detection(image):
-    # Change BRG image to Grayscale and RGB
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Performing OTSU threshold
-    ret, thresh1 = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-
-    # Establishing Kernel
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-
-    # Applying dilation on the threshold image
-    dilation = cv2.dilate(thresh1, kernel, iterations = 1)
-
-    # Find Contours
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # Creating a copy of image
-    im2 = image.copy()
-    
-    # A text file is created and flushed
-    file = open("recognized.txt", "w+")
-    file.write("")
-    file.close()
-    
-    # Looping through the identified contours
-    # Then rectangular part is cropped and passed on
-    # to pytesseract for extracting text from it
-    # Extracted text is then written into the text file
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        
-        # Drawing a rectangle on copied image
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
-        # Cropping the text block for giving input to OCR
-        cropped = im2[y:y + h, x:x + w]
-        
-        # Open the file in append mode
-        file = open("recognized.txt", "a")
-        
-        # Apply OCR on the cropped image
-        text = pytesseract.image_to_string(cropped)
-        
-        # Appending the text into file
-        file.write(text)
-        file.write("\n")
-        
-        # Close the file
-        file.close"""
-
 def finger_tracker(image, detection_result):
     """
     Draws all the landmarks on the hand
@@ -82,7 +23,6 @@ def finger_tracker(image, detection_result):
     """
     # Get image details
     imageHeight, imageWidth = image.shape[:2]
-
 
     # Get a list of the landmarks
     hand_landmarks_list = detection_result.hand_landmarks
@@ -113,13 +53,11 @@ def finger_tracker(image, detection_result):
         if pixelCoord:
             # Draw the circle around the index finger
             cv2.circle(image, (pixelCoord[0], pixelCoord[1]), 25, (255, 0, 0), 5)
-            pygame.draw.circle(screen, (255, 0, 0), (pixelCoord[0], pixelCoord[1]), 2, width=20)
+            pygame.draw.circle(screen, (redness, greenness, blueness), (pixelCoord[0] * 1.5, pixelCoord[1] * 1.5), thickness, width=20)
 
 """
 Start of main function
 """
-
-
 # Open facetime camera for video input
 video = cv2.VideoCapture(0)
 
@@ -132,10 +70,22 @@ detector = HandLandmarker.create_from_options(options)
 # Initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
+screen.fill((255, 255, 255))
 pygame.display.set_caption("Finger Art")
 
+running = True
+
+thickness = int(input("How thick do you want your pen to be?(number)"))
+redness = int(input("How much red do you want in your color?"))
+greenness = int(input("How much green do you want in your color?"))
+blueness = int(input("How much blue do you want in your color?"))
+
 # Loop until the end of the video
-while(video.isOpened()):
+while(video.isOpened() and running):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+           running = False
+        
     frame = video.read()[1]
 
     # Convert it to an RGB image
